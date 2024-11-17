e8b94671899094cbe7bbc08b16eeb720326af7b2
CCE107 <yoshbatula2@gmail.com>
2024-11-17 21:41:23 +0800

    designing with functionality

--- src/main/java/org/example/donmachos/LoginController.java
+++ src/main/java/org/example/donmachos/LoginController.java
@@ -1,6 +1,32 @@
 package org.example.donmachos;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+
+    @FXML
+    private TextField EmailAddressTF;
+
+    @FXML
+    private Button createBTN;
+
+    @FXML
+    private Button forgotpassBTN;
+
+    @FXML
+    private TextField passTF;
+
+    @FXML
+    private RadioButton rememberRBTN;
+
+    public void login (ActionEvent event){
+
+
+    }
 
 }
 
---
+++ src/main/java/org/example/donmachos/user.java
@@ -0,0 +1,25 @@
+package org.example.donmachos;
+
+ class User {
+     private String EmailAddress;
+     private String Username;
+     private String Password;
+
+     public User(String emailAddress, String username, String password) {
+         EmailAddress = emailAddress;
+         Username = username;
+         Password = password;
+     }
+
+     public String getEmailAddress() {
+         return EmailAddress;
+     }
+
+     public String getUsername() {
+         return Username;
+     }
+
+     public String getPassword() {
+         return Password;
+     }
+ }
