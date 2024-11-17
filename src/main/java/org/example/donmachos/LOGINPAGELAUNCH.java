21df4834734358e05f1a829d97a2b992a1e3aca0
CCE107 <yoshbatula2@gmail.com>
2024-11-17 21:02:56 +0800

    designing in login form part

--- src/main/java/org/example/donmachos/LOGINPAGELAUNCH.java
+++ src/main/java/org/example/donmachos/LOGINPAGELAUNCH.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class LOGINPAGELAUNCH extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(LOGINPAGELAUNCH.class.getResource("LOGIN.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("LOGIN FORM!");
         stage.setScene(scene);
---
+++ src/main/java/org/example/donmachos/LoginController.java
@@ -0,0 +1,8 @@
+package org.example.donmachos;
+
+public class LoginController {
+
+}
+
+
+
