07e9fa54b5c413dcd127301ace94e2053e033721
CCE107 <yoshbatula2@gmail.com>
2024-12-03 23:11:51 +0800

    yey

--- src/main/java/org/example/donmachos/summaryCont.java
+++ src/main/java/org/example/donmachos/summaryCont.java
@@ -1,6 +1,22 @@
 package org.example.donmachos;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+
 public class summaryCont {
 
+    @FXML
+    private Button cartOrderBTN;
+
+    @FXML
+    private GridPane orderSumarryGrid;
+
+    @FXML
+    private Text subtotaltext;
+
+    @FXML
+    private Text totalText;
 
 }
